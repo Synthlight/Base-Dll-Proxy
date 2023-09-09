2f4c47c4f54138040993c4cf75e0dadb32f84d94
Gregory Brutsche <gregory@mail.brutsches.local>
2023-09-08 22:20:11 -0400

    Add a 'vector contains' helper method.

--- Util.h
+++ Util.h
@@ -1,3 +1,8 @@
 #pragma once
 
 std::string GetExeFilename();
+
+template <class Container>
+const bool contains(const Container& container, const typename Container::value_type& element) {
+    return std::find(container.begin(), container.end(), element) != container.end();
+}
