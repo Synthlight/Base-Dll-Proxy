9c380cd8788baf322718e268174ab0f28b92dffc
Gregory Brutsche <gregory@mail.brutsches.local>
2024-07-30 12:31:20 -0400

    Fix win32 defines taking precedence over win64 defines.

--- Common.h
+++ Common.h
@@ -2,11 +2,11 @@
 
 
 #if _WIN32 || _WIN64
-#if _WIN32
-#define PTR_SIZE UINT32
-#define LARGEST_ADDRESS 0x7FFFFFFF
-#else
+#if _WIN64
 #define PTR_SIZE UINT64
 #define LARGEST_ADDRESS 0x7FFFFFFFFFFFFFFF
+#else
+#define PTR_SIZE UINT32
+#define LARGEST_ADDRESS 0x7FFFFFFF
 #endif
 #endif
